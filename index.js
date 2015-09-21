3a08fc441c6b910a1e907eac1fce0052aa9ec36f
Nguyen Trong Tin <nguyentrongtin2331@gmail.com>
2015-09-21 12:02:47 +0700

    push API

--- index.js
+++ index.js
@@ -1,13 +1,60 @@
 var express = require('express');
 var app = express();
+var http = require('http');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser());
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-
 app.get('/', function(request, response) {
   response.send("ok");
+});
+
+app.post('/push',function(request,response){
+  // Define relevant info
+  var privateKey = process.env.IONIC_PRIVATE_KEY;
+  var tokens = [];
+  // console.log($scope.token);
+  tokens.push(request.body.token);
+  var appId = process.env.IONIC_APP_ID;
+
+  // Encode your key
+  var auth = btoa(privateKey + ':');
+
+  // Build the request object
+  var req = {
+    method: 'POST',
+    host: 'https://push.ionic.io',
+    path: '/api/v1/push',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Ionic-Application-Id': appId,
+      'Authorization': 'basic ' + auth
+    },
+    data: {
+      "tokens": tokens,
+      "notification": {
+        "alert":"Hello World!"
+      }
+    }
+  };
+  // Make the API call
+  callback = function(res) {
+  var str = ''
+  res.on('data', function (chunk) {
+    str += chunk;
+  });
+
+  res.on('end', function () {
+    response.send(str);
+  });
+}
+
+  var pushRequest = http.request(req,callback);
+  pushRequest.end();
 });
 
 app.listen(app.get('port'), function() {
